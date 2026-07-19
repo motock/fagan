35902ec341cb64da9309919bdc25a57ea13536fa
Jesse Carroll <jessecarroll@Jesses-Mac-mini.local>
2026-07-19 14:23:15 -0500

    wip(dcce9211-ae96-4f01-ba01-3a2ece3a4908): interrupted

--- conftest.py
+++ conftest.py
@@ -4,9 +4,6 @@ import pytest
 @pytest.fixture(autouse=True)
 def _isolate_environ():
     snapshot = dict(os.environ)
-    for key in list(os.environ):
-        if key.startswith("PIPELINE_"):
-            del os.environ[key]
     yield
     os.environ.clear()
     os.environ.update(snapshot)
